1214a6ac83ffbea36c7793cd739387b6fee23f68
pravigya <pravigyajain5@gmail.com>
2022-06-08 18:06:18 +0530

    first comit

---
+++ Election.sol
@@ -0,0 +1,38 @@
+pragma solidity >=0.4.21 <0.7.0;
+
+contract Election {
+    // model a condidate
+    struct Candidate {
+        uint256 id;
+        string name;
+        uint256 voteCount;
+    }
+    // store accounts that have voted
+    mapping(address => bool) public voters;
+    // store a candiate
+    // fetch candidate
+    mapping(uint256 => Candidate) public candidates;
+    // store candidate count
+    uint256 public candidatesCount;
+
+    constructor() public {
+        addCandidate("candidate 1");
+        addCandidate("candidate 2");
+    }
+
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint256 _candidateId) public {
+        // address has not voted before
+        require(!voters[msg.sender]);
+        // voting for valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+        // update the voter has voted
+        voters[msg.sender] = true;
+        //update the votes of candidate
+        candidates[_candidateId].voteCount++;
+    }
+}
---
+++ Lottery.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.11;
+
+contract Lottery {
+    address public owner;
+    address payable[] public players;
+    uint256 public lotteryId;
+    mapping(uint256 => address payable) public lotteryHistory;
+
+    constructor() {
+        owner = msg.sender;
+        lotteryId = 1;
+    }
+
+    function getWinnerByLottery(uint256 lottery)
+        public
+        view
+        returns (address payable)
+    {
+        return lotteryHistory[lottery];
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function getPlayers() public view returns (address payable[] memory) {
+        return players;
+    }
+
+    function enter() public payable {
+        require(msg.value > .01 ether);
+
+        // address of player entering lottery
+        players.push(payable(msg.sender));
+    }
+
+    function getRandomNumber() public view returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(owner, block.timestamp)));
+    }
+
+    function pickWinner() public onlyowner {
+        uint256 index = getRandomNumber() % players.length;
+        players[index].transfer(address(this).balance);
+
+        lotteryHistory[lotteryId] = players[index];
+        lotteryId++;
+
+        // reset the state of the contract
+        players = new address payable[](0);
+    }
+
+    modifier onlyowner() {
+        require(msg.sender == owner);
+        _;
+    }
+}
